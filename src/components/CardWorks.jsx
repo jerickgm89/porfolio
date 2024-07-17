226014c3874db9b7e7fe6170486423ea28951cba
JErickDev <jerickgm89@gmail.com>
2024-07-16 20:57:49 -0500

    Add: Details ecommerceTech

--- src/components/CardWorks.jsx
+++ src/components/CardWorks.jsx
@@ -7,11 +7,11 @@ import {
   Link,
 } from "@nextui-org/react";
 
-export const CardWorks = ({ title, image, description }) => {
+export const CardWorks = ({ title, image, description, url }) => {
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-4">
-        <Link href="#" underline="none">
+        <Link href={url} underline="none">
           <Card isBlurred className="py-2 md:py-4">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
               <p className="text-xl font-bold">{title}</p>
