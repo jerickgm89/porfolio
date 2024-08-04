bae97028f5f1631d3f21febfa6c0f8c64fbfcae1
JErickDev <jerickgm89@gmail.com>
2024-08-04 01:56:06 -0500

    Refactor: Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), icon(), mdx()],
-  prefetch: true
 });
+
